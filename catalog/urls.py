e4f8625abb703f968e147628053f49a14662ca16
ravcode9 <rlatypov2106@gmail.com>
2024-03-12 10:36:08 +0500

    new project

--- catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, home, contacts
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('home/', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-]
