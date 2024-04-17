3eeda9d0ea1f8f29d26f6e72e85f438c91ae6af0
ravcode9 <rlatypov2106@gmail.com>
2024-04-17 15:16:24 +0500

    20.2 dz

--- catalog/templatetags/media_tags.py
+++ catalog/templatetags/media_tags.py
@@ -8,3 +8,8 @@ def media_url(path):
     if path:
         return f'/media/{path}'
     return '/static/product.jpg'
+
+
+@register.filter
+def get_active_version(product_versions, product):
+    return product_versions.get(product)
